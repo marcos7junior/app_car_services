3d4941b9dcbfc2208d2c96c64cecc581246f7475
Marcos Paiva <marcospaivajr7@gmail.com>
2023-08-31 13:18:31 -0300

    feat: created model Vehicles on models.py

--- car_services/models.py
+++ car_services/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Vehicles(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    marca = models.CharField(max_length=15, null=False)
+    modelo = models.CharField(max_length=15, null=False)
+    ano = models.IntegerField(null=False)
+
+    def __str__(self):
+        return f'{self.usuario} - {self.marca} - {self.modelo}'
+    
+    
