59d4ac2ca48535a31b409ec049ac000ca52e9fba
Débora Christyna <cdias.proj@gmail.com>
2023-09-04 10:20:46 -0300

    implementa user

--- car_services/models.py
+++ car_services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    nome = models.CharField(max_length=50)
+    dnascimento = models.IntegerField(default=0)
+    mascimento = models.CharField(max_length=10, default='null')
+    anascimento = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nome
+
+
--- car_services/views.py
+++ car_services/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import User
 
-# Create your views here.
+def home(request):
+    usuarios = User.objects.all()
+    return render(request, 'index.html', {"usuario": usuarios})
